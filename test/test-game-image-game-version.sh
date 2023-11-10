b6601b27f3aa2ef7191c0e95e56b4e472c182975
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-11-10 19:52:31 +0000

    Add test

--- test/test-game-image-game-version.sh
+++ test/test-game-image-game-version.sh
@@ -4,5 +4,10 @@ set -eu
 SCRIPT_DIR=$( cd  $( dirname "$0" ) && pwd )
 cd "$SCRIPT_DIR"
 
-for i in $( ls hlds-* ); do echo "[$i]"; cat "$i" | grep -iE '\bexe\b|version' | sed 's/[^0-9]//g'; echo; done
-for i in $( ls srcds-* ); do echo "[$i]"; cat "$i" | grep -iE '\bexe\b|version' | sed 's/[^0-9]//g'; echo; done
+for i in $( ls hlds-* srcds-* | grep -v srcds-cs2 ); do
+    echo "[$i]"
+    cat "$i" | grep -iE '\bexe\b|version'
+    echo
+    cat "$i" | grep -iE '\bexe\b|version' | sed 's/[^0-9]//g'
+    echo
+done
