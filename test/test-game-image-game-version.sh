777e36b8a0257b469f4b7ac244e07ff307ce1e27
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-11-10 20:00:07 +0000

    Add test

--- test/test-game-image-game-version.sh
+++ test/test-game-image-game-version.sh
@@ -11,3 +11,11 @@ for i in $( ls hlds-* srcds-* | grep -v srcds-cs2 ); do
     cat "$i" | grep -iE '\bexe\b|version' | sed 's/[^0-9]//g'
     echo
 done
+
+for i in srcds-cs2-query; do
+    echo "[$i]"
+    cat "$i" | tr "[:cntrl:]" "\\n" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
+    echo
+    cat "$i" | tr "[:cntrl:]" "\\n" | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sed 's/[^0-9]//g'
+    echo
+done
